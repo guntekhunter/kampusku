589f89141de3df1370d28c85b8f3242d7a274140
guntekhunter <72636466+guntekhunter@users.noreply.github.com>
2021-09-27 10:52:52 +0800

    tambahan fungsi run halaman code editor

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const kiri = document.querySelector(".kiri"),
     kanan = document.querySelector(".kanan"),
     bar = document.querySelector(".bar"),
-    editor = document.querySelector(".editor")
+    editor = document.querySelector(".editor"),
+    run = document.querySelector(".run-btn"),
+    iframe = document.querySelector(".iframe");
 
 const drag = (e) => {
     e.preventDefault();
@@ -12,8 +14,15 @@ const drag = (e) => {
 }
 bar.addEventListener("mousedown", () => {
     document.addEventListener("mousemove", drag);
-    document.style.width = (auto);
 })
 bar.addEventListener("mouseup", () => {
     document.removeEventListener("mousemove", drag);
+})
+// tombol run
+run.addEventListener("click", () => {
+    console.log("ommaleka");
+    const html = editor.textContent;
+    iframe.style.color = ("white");
+    iframe.src = "data:text/html;charset=utf-8," + encodeURI
+        (html);
 })
