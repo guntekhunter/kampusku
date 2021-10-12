19f7610ec4306ff308a9f428337e0cef3b36b1c2
guntekhunter <72636466+guntekhunter@users.noreply.github.com>
2021-10-12 21:19:18 +0800

    adding codigniter

---
+++ application/views/template/black-header.php
@@ -0,0 +1,10 @@
+<section class="dark-header">
+    <div class="container container-artikel">
+        <div class="header-page">
+            <h1 class="putih">Code Editor</h1>
+            <h3 class="putih">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam cum id dolorum natus
+                molestias qui.
+            </h3>
+        </div>
+    </div>
+</section>
